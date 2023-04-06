6d0b3acf73c2d6df31f6124ebd9f4a43a83ca5e9
Rasul <rasulrizvan3@gmail.com>
2023-04-06 14:57:42 +0600

    добавил дозагрузку всех персонажей

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,52 +1,50 @@
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import "./charList.scss";
+/* import abyss from "../../resources/img/abyss.jpg"; */
+import { useEffect, useState } from "react";
+import MarvelService from "../services/MarvelServices";
 
 const CharList = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const marvelService = new MarvelService();
+
+    const onCharsLoaded = (res) => {
+      setItems(res);
+    };
+    const getAll = () => {
+      marvelService.getAllCharacters().then(onCharsLoaded);
+    };
+    getAll();
+  }, []);
+
+  const elements = items.map((item) => {
+    let imgStyle = { objectFit: "cover" };
+    if (
+      item.thumbnail ===
+      "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+    ) {
+      imgStyle = { objectFit: "unset" };
+    }
     return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item char__item_selected">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-                <li className="char__item">
-                    <img src={abyss} alt="abyss"/>
-                    <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
-}
+      <li className="char__item" key={item.id}>
+        <a href={item.wiki}>
+          {" "}
+          <img src={item.thumbnail} alt={item.name} style={imgStyle} />
+          <div className="char__name">{item.name}</div>
+        </a>
+      </li>
+    );
+  });
+
+  return (
+    <div className="char__list">
+      <ul className="char__grid">{elements}</ul>
+      <button className="button button__main button__long">
+        <div className="inner">load more</div>
+      </button>
+    </div>
+  );
+};
 
 export default CharList;
--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -18,14 +18,13 @@ const RandomChar = () => {
     setLoading(false);
     setError(true);
   };
+  const updateChar = () => {
+    const id = Math.floor(Math.random() * (1011400 - 1011000)) + 1011000;
+    marvelService.getCharacter(id).then(onCharLoaded).catch(onError);
+    /*     console.log(id); */
+  };
   useEffect(() => {
-    const updateChar = () => {
-      const id = Math.floor(Math.random() * (1011400 - 1011000)) + 1011000;
-      marvelService.getCharacter(id).then(onCharLoaded).catch(onError);
-    };
-    const elems = updateChar();
-
-    return () => elems;
+    updateChar();
   }, []);
 
   const Error = error ? <ErrorMessage></ErrorMessage> : null;
@@ -44,7 +43,9 @@ const RandomChar = () => {
         </p>
         <p className="randomchar__title">Or choose another one</p>
         <button className="button button__main">
-          <div className="inner">try it</div>
+          <div className="inner" onClick={updateChar}>
+            try it
+          </div>
         </button>
         <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
       </div>
@@ -54,9 +55,21 @@ const RandomChar = () => {
 
 const View = ({ char }) => {
   const { name, description, thumbnail, homepage, wiki } = char;
+  let imgStyle = { objectFit: "cover" };
+  if (
+    thumbnail ===
+    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+  ) {
+    imgStyle = { objectFit: "contain" };
+  }
   return (
     <div className="randomchar__block">
-      <img src={thumbnail} alt="Random character" className="randomchar__img" />
+      <img
+        src={thumbnail}
+        alt="Random character"
+        className="randomchar__img"
+        style={imgStyle}
+      />
       <div className="randomchar__info">
         <p className="randomchar__name">{name}</p>
         <p className="randomchar__descr">{description}</p>
